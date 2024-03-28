d0f25112dd1f99abe98b49bccd2ebd492d72dd09
Ewerton Dutra <ewerton-speed@hotmail.com>
2024-03-28 11:28:00 -0300

    Terceira aula, fizemos a tela de Home

---
+++ lib/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.menu),
+        title: const Text('Impressor Delivery (Código)'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.refresh),
+          ),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.logout)),
+        ],
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Só aguardar os pedidos...',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'A conexão em real-time está ativa.',
+              style: TextStyle(fontSize: 14),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
